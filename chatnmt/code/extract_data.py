b43c0ec21608600a3dfef4ba813bc02a43f055b7
Chujun Song <a925504340@gmail.com>
2020-06-16 18:00:42 -0400

    fixed preprocessed files for multi_encoder and apply iwlst14 bpe model to preprocessed files

--- chatnmt/code/extract_data.py
+++ chatnmt/code/extract_data.py
@@ -1,60 +1,70 @@
 import json
 
 # Opening JSON file
-PATH = "../origdata/train.json"
-f = open(PATH,)
-en_file_path = "../data/train.en"
-de_file_path = "../data/train.de"
+def extract_data(json_file_path,extracted_file_path, split_training=True, add_remove_boundry=True):
+    # PATH = "../origdata/train.json"
+    f = open(json_file_path,)
+    en_file_path = extracted_file_path + 'en'
+    de_file_path = extracted_file_path + 'de'
+    f_en = open(en_file_path,'w')
+    f_de = open(de_file_path,'w')
 
-f_en = open(en_file_path,'w')
-f_de = open(de_file_path,'w')
-
-
-
-# returns JSON object as
-# a dictionary
-chats = json.load(f)
-
-# Iterating through the json
-# list
-for chat in chats.values():
-    wrote = False
-    prev_speaker = chat[0]['speaker']
-    en_tmp = ""
-    de_tmp = ""
-    for turn in chat:
-        if turn['speaker']!=prev_speaker:
-            wrote = True
-            f_en.write(en_tmp)
-            f_en.write('\n')
-            f_de.write(de_tmp)
-            f_de.write('\n')
-            if turn['speaker']=="agent":
-                en_tmp = turn['source']
-                de_tmp = turn['target']
+    # returns JSON object as
+    # a dictionary
+    chats = json.load(f)
+    i = 0
+    for chat in chats.values():
+        i = i + 1
+        if split_training:
+            if i>495:
+                f_en = open(en_file_path[:-5] +'test' + 'en','w')
+                f_de = open(en_file_path[:-5] +'test' + 'de','w')
+        wrote = False
+        prev_speaker = chat[0]['speaker']
+        en_tmp = ""
+        de_tmp = ""
+        for turn in chat:
+            if turn['speaker']!=prev_speaker:
+                wrote = True
+                f_en.write(en_tmp)
+                f_en.write('\n')
+                f_de.write(de_tmp)
+                f_de.write('\n')
+                if turn['speaker']=="agent":
+                    en_tmp = turn['source']
+                    de_tmp = turn['target']
+                else:
+                    en_tmp = turn['target']
+                    de_tmp = turn['source']
             else:
-                en_tmp = turn['target']
-                de_tmp = turn['source']
-        else:
-            if turn['speaker']=="agent":
-                en_tmp+=turn['source']
-                de_tmp+=turn['target']
-            else:
-                en_tmp+= turn['target']
-                de_tmp+= turn['source']
-        prev_speaker = turn['speaker']
-    # print end of convo, for boundaries
+                if turn['speaker']=="agent":
+                    en_tmp+=turn['source']
+                    de_tmp+=turn['target']
+                else:
+                    en_tmp+= turn['target']
+                    de_tmp+= turn['source']
+            prev_speaker = turn['speaker']
+        # print end of convo, for boundaries
 
-    if wrote:
-        f_en.write('REMOVEMEIMABOUNDARY\n')
-        f_de.write('REMOVEMEIMABOUNDARY\n')
-f.close()
-f_en.close()
-f_de.close()
+        if wrote and add_remove_boundry:
+            f_en.write('REMOVEMEIMABOUNDARY\n')
+            f_de.write('REMOVEMEIMABOUNDARY\n')
+    f.close()
+    f_en.close()
+    f_de.close()
 
 
+def main():
+    #generate training file and testing file aligned line by line
+    # testing file is split from training file, the ration is about 9:1
+    json_file_path = "../origdata/train.json"
+    extracted_file_path = "../data/train"
+    extract_data(json_file_path, extracted_file_path)
+
+    #generate dev file aligned line by line
+    json_file_path = "../origdata/dev.json"
+    extracted_file_path = "../data/dev"
+    extract_data(json_file_path, extracted_file_path,split_training = False)
 
 
 
-# Closing file
-f.close()
