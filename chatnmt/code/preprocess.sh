efce6ba900aca96c3eb6e2385dae18abdf390997
Chujun Song <a925504340@gmail.com>
2020-06-30 22:31:51 -0400

    re-preprocess data with normalization

--- chatnmt/code/preprocess.sh
+++ chatnmt/code/preprocess.sh
@@ -14,15 +14,18 @@
 MOSES=`pwd`/../../mosesdecoder
 
 SCRIPTS=${MOSES}/scripts
+NORMALIZER=${SCRIPTS}/tokenizer/normalize-punctuation.perl
+REMOVER=${SCRIPTS}/tokenizer/remove-non-printing-char.perl
 TOKENIZER=${SCRIPTS}/tokenizer/tokenizer.perl
 LC=${SCRIPTS}/tokenizer/lowercase.perl
 CLEAN=${SCRIPTS}/training/clean-corpus-n.perl
 
-merge_ops=iwslt14-deen-bpe
+
+merge_ops=wmt_ende_best
 src=de
 tgt=en
 lang=de-en
-prep="../multi_encoder"
+prep="../prep"
 # tmp=${prep}/tmp
 orig="../data"
 # prep = "../prep"
@@ -33,37 +36,61 @@ train=train
 test=test
 dev=dev
 
-# codes_file="../bpe/iwslt14-deen-bpe.32000"
-codes_file="${prep}/bpe.${merge_ops}"
+codes_file="../bpe/bpe.model"
+# codes_file="${prep}/bpe.${merge_ops}"
 
 echo "pre-processing train data..."
+
+echo "normalizing data..."
 for l in ${src} ${tgt}; do
-    for p in ${train} ${dev} ${test}; do
+    for p in ${train} ${test} ${dev}; do
         f=${p}.$l
-        tok=${p}.tok.$l
+        norm=${p}.norm.$l
         cat ${orig}/${f} | \
+        perl ${NORMALIZER} -threads 8 -l $l > ${prep}/${norm}
+        echo ""
+    done
+done
+
+echo "removing non printable chars..."
+for l in ${src} ${tgt}; do
+    for p in ${train} ${test} ${dev}; do
+        f=${p}.$l
+        rem=${p}.rem.$l
+        norm=${p}.norm.$l
+        cat ${prep}/${norm} | \
+        perl ${REMOVER} -threads 8 -l $l > ${prep}/${rem}
+        echo ""
+    done
+done
+
+for l in ${src} ${tgt}; do
+    for p in ${train} ${test} ${dev}; do
+        rem=${p}.rem.$l
+        tok=${p}.tok.$l
+        cat ${prep}/${rem} | \
         perl ${TOKENIZER} -threads 8 -l $l > ${prep}/${tok}
         echo ""
     done
 done
-for p in ${train} ${dev} ${test}; do
+for p in ${train} ${test} ${dev}; do
     perl ${CLEAN} -ratio 9 ${prep}/${p}.tok ${src} ${tgt} ${prep}/${p}.clean 1 80
 done
 for l in ${src} ${tgt}; do
-    for p in ${train} ${dev} ${test}; do
-        perl ${LC} < ${prep}/${p}.clean.${l} > ${prep}/${p}.tags.${l}
+    for p in ${train} ${test} ${dev} ; do
+        cp ${prep}/${p}.clean.${l}  ${prep}/${p}.tags.${l}
     done
 done
 
-echo "learning * joint * BPE..."
-codes_file="${prep}/bpe.${merge_ops}"
-cat "${prep}/${train}.tags.${src}" "${prep}/${train}.tags.${tgt}" > ${prep}/${train}.tmp
-python3 -m subword_nmt.learn_bpe -s "${merge_ops}" -i "${prep}/${train}.tmp" -o "${codes_file}"
-# rm "${prep}/train.tmp"
+# echo "learning * joint * BPE..."
+# codes_file="${prep}/bpe.${merge_ops}"
+# cat "${prep}/${train}.tags.${src}" "${prep}/${train}.tags.${tgt}" > ${prep}/${train}.tmp
+# python3 -m subword_nmt.learn_bpe -s "${merge_ops}" -i "${prep}/${train}.tmp" -o "${codes_file}"
+# # rm "${prep}/train.tmp"
 
 echo "applying BPE..."
 for l in ${src} ${tgt}; do
-    for p in ${train} ${dev} ${test}; do
+    for p in ${train} ${test} ${dev}; do
         python3 -m subword_nmt.apply_bpe -c "${codes_file}" -i "${prep}/${p}.tags.${l}" -o "${prep}/${p}.tags.bpe.${merge_ops}.${l}"
     done
 done
