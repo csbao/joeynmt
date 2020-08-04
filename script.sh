0614e30923bf4fda3558fd27e538a616b4fb5a11
Calvin Bao <csbao@cs.umd.edu>
2020-08-04 08:05:24 -0400

    Adding script

---
+++ script.sh
@@ -0,0 +1,28 @@
+arr=()
+arr+=('configs/viznet/2_2_0.1_128_128_128.yml')
+arr+=('configs/viznet/2_2_0.1_128_128_64.yml')
+arr+=('configs/viznet/2_2_0.1_128_64_256.yml')
+arr+=('configs/viznet/2_2_0.1_64_128_128.yml')
+arr+=('configs/viznet/2_2_0.1_64_128_64.yml')
+arr+=('configs/viznet/2_2_0.1_64_64_256.yml')
+arr+=('configs/viznet/2_2_0.2_128_128_128.yml')
+arr+=('configs/viznet/2_2_0.1_128_128_256.yml')
+arr+=('configs/viznet/2_2_0.1_128_64_128.yml')
+arr+=('configs/viznet/2_2_0.1_128_64_64.yml')
+arr+=('configs/viznet/2_2_0.1_64_128_256.yml')
+arr+=('configs/viznet/2_2_0.1_64_64_128.yml')
+arr+=('configs/viznet/2_2_0.1_64_64_64.yml')
+i=$((0))
+for filename in configs/viznet/attempt2/*.yml; do
+  i=$(($i + 1));
+  echo $i
+
+  if [[ " ${arr[*]} " == *"$filename"* ]];
+  then
+      continue
+  else
+      python3 -m joeynmt train $filename;
+  fi
+    # ... rest of the loop body
+done
+
